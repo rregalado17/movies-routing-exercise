0f45c688bb44ed40d6f373e73900211f631cf053
rregalado17 <rregalado17@gmail.com>
2021-08-06 00:27:58 -0400

    movies routing exercise

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,20 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const moviesList = movies.map((movie) => (
+    <ul key={movie.title}>
+      Title: {movie.title}
+      <p>Time: {movie.time} minutes</p>
+      <p>Genre: {(movie.genres).map((genre) => {
+        return (
+          <li key={genre}>{genre}</li>
+        )
+      })}</p>
+    </ul>
+  ))
+
+  return <div><h1>Movies Page</h1>{moviesList}</div>;
 }
 
 export default Movies;
