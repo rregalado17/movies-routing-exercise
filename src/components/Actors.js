1ce932388e12296d9463baa008b14e57edfd9b76
rregalado17 <rregalado17@gmail.com>
2021-08-06 17:15:00 -0400

    movie app completed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,29 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const actorName = actors.map((actor, index) => {
+    return (
+      <div key={index}>
+        <p>Actor's Name: {actor.name} <br/>
+        Movies: {(actor.movies).map((m, index) => {
+          return (
+            <li key={index}>
+              {m}
+            </li>
+          )
+        })}</p>
+      </div>
+    )
+  }
+  )
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorName}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,29 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  
+  const directorName = directors.map((director, index) => {
+      return (
+        <div key={index}>
+          <p>Director's Name: {director.name} <br/>
+          Movies: {(director.movies).map((m, index) => {
+            return (
+              <li key={index}>
+                {m}
+              </li>
+            )
+          })} </p>
+        </div>
+      )
+    }
+  )
+  
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorName}
+    </div>
+  );
 }
 
 export default Directors;
